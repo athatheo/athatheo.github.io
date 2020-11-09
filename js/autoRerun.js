1a1f4b2f9cc5d4704683f128dd847bc22f267404
athatheo <athatheoc@gmail.com>
2020-11-09 19:02:30 +0200

    Fixed a bug in what-if questions caused by change of variable names

--- js/autoRerun.js
+++ js/autoRerun.js
@@ -29,7 +29,7 @@ function re_run(e, if_regenerate=true) {
 	}
 
     var inputIdList=[
-        "N","E","F","qS","qL","insert","blind_update","read_modify_update","r","query_count","v","s","cost"
+        "N","E","F","qS","qL","insert_workload","blind_update_workload","read_modify_update_workload","r","query_count","v","s","cost"
     ]
 
     for(var i=0;i<inputIdList.length;i++) {
@@ -483,6 +483,7 @@ function switchText(id) {
 }
 
 function checkInput(input){
+    console.log(input.value);
     if(isNaN(parseFloat(input.value))){
         alert("Invalid input!");
         return false;
